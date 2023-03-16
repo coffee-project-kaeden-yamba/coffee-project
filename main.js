21b4c9538e8527d6c48d7b2fb13ecd1db7b59b94
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-03-16 08:51:49 -0500

    reconfigure

--- main.js
+++ main.js
@@ -1,39 +1,4 @@
 "use strict"
-//////////// START MOBILE MENU ///////////
-let pageWrapper = document.querySelector('.page-wrapper');
-let toggleMenu = document.querySelector('.das-burger-wrapper');
-let menuBg = document.querySelector('.mobile-menu-overlay');
-toggleMenu.addEventListener('click', function(){
-    pageWrapper.classList.toggle('mobile-menu-open');
-});
-menuBg.addEventListener('click', function(){
-    pageWrapper.classList.toggle('mobile-menu-open');
-});
-//////////// END MOBILE MENU ///////////
-
-//////////// START DROPDOWNS ///////////
-let dropdowns = document.querySelectorAll('[data-dropdown="parent"]');
-dropdowns.forEach(function(dropdown){
-    let toggle = dropdown.querySelector('[data-dropdown="toggle"]');
-    toggle.addEventListener('click', function(){
-        dropdowns.forEach(function(element){
-            // if already has class of open and is not this element, remove it
-            if (element.classList.contains('open') && element !== dropdown) {
-                element.classList.remove('open');
-            }
-        })
-        dropdown.classList.toggle('open');
-    });
-});
-pageWrapper.addEventListener('click', function(event){
-    //if the event target is not a dropdown, close all dropdowns
-    if (!event.target.closest('[data-dropdown="parent"]')) {
-        dropdowns.forEach(function(dropdown){
-            dropdown.classList.remove('open');
-        });
-    }
-});
-//////////// END DROPDOWNS ///////////
 
 // Displays Coffee onh HTML by name and roast type
 function renderCoffee(coffee) {
